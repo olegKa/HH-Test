b5f401fdb709157ba4688c3612676175d4084507
olegKa <kalinin_oleg@list.ru>
2015-10-19 19:05:36 +0500

    Initial Commit

---
+++ hh-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hh-test
+//
+//  Created by OLEG KALININ on 19.10.15.
+//  Copyright © 2015 OLEG KALININ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hh-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hh-test
+//
+//  Created by OLEG KALININ on 19.10.15.
+//  Copyright © 2015 OLEG KALININ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
